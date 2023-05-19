314e67f726af0b43df21df49030f98416ddab1d5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-05-19 12:58:16 +0200

    Remove BE APIs

--- include/utf.h
+++ include/utf.h
@@ -25,13 +25,7 @@ size_t
 utf8_length_from_utf16le (const utf16_t *data, size_t len);
 
 size_t
-utf8_length_from_utf16be (const utf16_t *data, size_t len);
-
-size_t
 utf8_convert_to_utf16le (const utf8_t *data, size_t len, utf16_t *result);
-
-size_t
-utf8_convert_to_utf16be (const utf8_t *data, size_t len, utf16_t *result);
 
 /**
  * UTF-16
@@ -40,17 +34,11 @@ utf8_convert_to_utf16be (const utf8_t *data, size_t len, utf16_t *result);
 bool
 utf16le_validate (const utf16_t *data, size_t len);
 
-bool
-utf16be_validate (const utf16_t *data, size_t len);
-
 size_t
 utf16_length_from_utf8 (const utf8_t *data, size_t len);
 
 size_t
 utf16le_convert_to_utf8 (const utf16_t *data, size_t len, utf8_t *result);
-
-size_t
-utf16be_convert_to_utf8 (const utf16_t *data, size_t len, utf8_t *result);
 
 #ifdef __cplusplus
 }
