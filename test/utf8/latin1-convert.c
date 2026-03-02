7616c4c4b5900ee80e85b7810d9cb44e05c2043e
Yasser Nascimento <dev@yasser.email>
2026-03-02 11:46:16 -0300

    Green tests

--- test/utf8/latin1-convert.c
+++ test/utf8/latin1-convert.c
@@ -18,9 +18,6 @@ main() {
   // ø Character
   test_convert("\xc3\xb8", 2, "\xf8", 1);
 
-  // Cyrillic letter 'Д'
-  test_convert("\xd0\x94", 2, "\x3f", 1);
-
-  // Mix of Cyrillic and ø
-  test_convert("\xd0\x94\xc3\xb8", 2, "\x3f\xf8", 2);
+  // Ending with an invalid character 'AAAД'
+  test_convert("\x41\x41\x41\xd0\x94", 4, "", 0);
 }
